da9e3764a0e83eea2923ca747480545e479d5d86
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2018-05-08 15:31:55 +0100

    merge to master

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -4,11 +4,11 @@ let header = Vue.component("header-component",{
 	template: 	`<div>
 					<div style="overflow: hidden; height: 100%;" id="scene">
 						<img class="backgroundScafold" src="app/assets/images/WebLabs-Fondo-azul.png"/>
-						<img data-depth="0.4" data-limit-x="true" class="backGroundImage" src="app/assets/images/WebLabs-sombra2.png"/>
-						<img data-depth="0.8" data-limit-x="true" class="backGroundImage" src="app/assets/images/WebLabs-sombra1.png"/>
-						<img data-depth="0.9" data-limit-x="true" class="backGroundImage" src="app/assets/images/WebLabs-Ilustracion-probetas.png"/>
-						<img data-depth="2" data-limit-x="true" class="backGroundImage" src="app/assets/images/WebLabs-Ilustracion-postit.png"/>
-						<img data-depth="1.001" data-limit-x="true" class="backGroundImage" src="app/assets/images/WebLabs-Ilustracion-llamas.png"/>
+						<img data-depth="0.4" data-limit-x="true" class="backGroundImage" src="app/assets/images/webLabs-sombra2.png"/>
+						<img data-depth="0.8" data-limit-x="true" class="backGroundImage" src="app/assets/images/webLabs-sombra1.png"/>
+						<img data-depth="0.9" data-limit-x="true" class="backGroundImage" src="app/assets/images/webLabs-ilustracion-probetas.png"/>
+						<img data-depth="2" data-limit-x="true" class="backGroundImage" src="app/assets/images/webLabs-ilustracion-postit.png"/>
+						<img data-depth="1.001" data-limit-x="true" class="backGroundImage" src="app/assets/images/webLabs-ilustracion-llamas.png"/>
 					</div>
 					<div class="general">
 						<div class="lasbWaterMark">Web Labs | BEEVA</div>
